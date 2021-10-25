73e84539b2829433456532508579cdf0dee684f9
mabunde <solomonsitati39@gmail.com>
2021-10-25 23:15:29 +0300

    prompt the user to enter the plain text to encode

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,5 +3,7 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args){
         Scanner in =new Scanner(System.in);
+        System.out.println("Enter plain text >>");
+        String plain =in.nextLine();
     }
 }
